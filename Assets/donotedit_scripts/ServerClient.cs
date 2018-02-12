b67a04bab08f8d3559578a945717fd502832260d
NinaPostolov <nin4ikp@mail.ru>
2018-02-12 10:32:53 +0100

    Backup before updating Unity from 5.6.1f1 to 2017.3.1

---
+++ Assets/donotedit_scripts/ServerClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerClient : MonoBehaviour {
+
+    void Start()
+    {
+        StartCoroutine(Upload());
+    }
+
+    IEnumerator Upload()
+    {
+        WWWForm formData = new WWWForm();
+        //formData.Add(new MultipartFormDataSection("field1=foo&field2=bar"));
+        formData.AddField("Player data", "playerInfo.dat"); //Application.persistentDataPath + "/playerInfo.dat"
+
+        UnityWebRequest www = UnityWebRequest.Post("ws://echo.websocket.org/", formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+    }
+}
